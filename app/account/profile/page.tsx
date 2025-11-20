9d3b8a905f4f60b713d05cad6f5903e888f91df5
tsing kou ng <paulngyn512@gmail.com>
2025-11-20 14:25:44 -0800

    v check

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -1,24 +1,8 @@
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
 import NameForm from '@/components/ui/AccountForms/NameForm';
 import EmailForm from '@/components/ui/AccountForms/EmailForm';
 import Link from 'next/link';
-import {
-  getUserDetails,
-  getUser
-} from '@/utils/supabase/queries';
 
 export default async function ProfilePage() {
-  const supabase = createClient();
-  const [user, userDetails] = await Promise.all([
-    getUser(supabase),
-    getUserDetails(supabase)
-  ]);
-
-  if (!user) {
-    return redirect('/signin');
-  }
-
   return (
     <div className="min-h-screen bg-white py-8">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -43,8 +27,8 @@ export default async function ProfilePage() {
 
         {/* Forms */}
         <div className="space-y-8">
-          <NameForm userName={userDetails?.full_name ?? ''} />
-          <EmailForm userEmail={user.email} />
+          <NameForm userName={''} />
+          <EmailForm userEmail={''} />
         </div>
       </div>
     </div>
