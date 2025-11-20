07e01bda8fdb8ffb11f86eb381ee1bac26f8cf4d
tsing kou ng <paulngyn512@gmail.com>
2025-11-20 14:23:08 -0800

    v check

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,25 +1,7 @@
 import CustomerPortalForm from '@/components/ui/AccountForms/CustomerPortalForm';
-import { redirect } from 'next/navigation';
-import { createClient } from '@/utils/supabase/server';
-import {
-  getUserDetails,
-  getSubscription,
-  getUser
-} from '@/utils/supabase/queries';
 
 export default async function Account() {
-  const supabase = createClient();
-  const [user, userDetails, subscription] = await Promise.all([
-    getUser(supabase),
-    getUserDetails(supabase),
-    getSubscription(supabase)
-  ]);
-
-  if (!user) {
-    return redirect('/signin');
-  }
-
   return (
-    <CustomerPortalForm subscription={subscription} user={user} />
+    <CustomerPortalForm subscription={null} user={null} />
   );
 }
